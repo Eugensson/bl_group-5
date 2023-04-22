5ef4928f76661ce32eb8aab3b82b4c0863c98ec7
M. A. Startsev <m.a.startsev@gmail.com>
2023-04-22 21:18:25 +0300

    todos

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,24 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text, counter, onClick, id, disabled }) => {
+  return (
+    <>
+      <TodoWrapper>
+        <Text textAlign="center" marginBottom="20px">
+          TODO #{counter}
+        </Text>
+
+        <Text>{text}</Text>
+
+        <DeleteButton
+          type="button"
+          onClick={() => onClick(id)}
+          disabled={disabled}
+        >
+          <RiDeleteBinLine size={24} />
+        </DeleteButton>
+      </TodoWrapper>
+    </>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,42 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  onHandleSubmit = qwery => {
+    const todo = {
+      id: nanoid(),
+      text: qwery,
+    };
+
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
+  handleDeleteTodo = id => {
+    this.setState({ todos: this.state.todos.filter(item => item.id !== id) });
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.onHandleSubmit} />
+        <Grid>
+          {todos.length > 0 &&
+            todos.map(({ id, text }, index) => (
+              <GridItem key={id}>
+                <Todo
+                  id={id}
+                  text={text}
+                  counter={index + 1}
+                  onClick={this.handleDeleteTodo}
+                />
+              </GridItem>
+            ))}
+        </Grid>
+      </>
+    );
   }
 }
