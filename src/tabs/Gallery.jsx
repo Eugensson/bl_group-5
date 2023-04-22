3ee8e074606b9e1f15cfd1b7e9e25ff4b35a2784
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-04-22 20:47:02 +0300

    task 1 done

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,27 +8,55 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     photos: [],
+    isShowButton: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
       ImageService.getImages(this.state.query, this.state.page).then(response =>
         this.setState(prevState => ({
           photos: [...prevState.photos, ...response.data.photos],
+          isShowButton:
+            response.data.page <
+            Math.ceil(response.data.total_results / response.data.per_page),
         }))
       );
     }
   }
 
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
   onHandleSubmit = query => {
-    this.setState({ query });
+    this.setState({ query, photos: [], isShowButton: false, page: 1 });
   };
 
   render() {
+    const { photos, isShowButton } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onHandleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <Grid>
+          {photos.length > 0 &&
+            photos.map(({ id, avg_color, alt, src: { large } }) => (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            ))}
+        </Grid>
+        {isShowButton && <Button onClick={this.loadMore} />}
+
+        {photos.length === 0 && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
       </>
     );
   }
